9e4d12791f0a1f7b980684d09cb17c36a6b22ded
Alon Albert <aalbert@google.com>
2018-12-17 11:20:49 +0200

    Add Next/Prev

--- create-lyrics-html.py
+++ create-lyrics-html.py
@@ -4,11 +4,17 @@ import sys
 
 from os import path, listdir
 from shutil import copyfile
+import copy
+
+NEXT_FILE = '{next-file}'
+NEXT_NAME = '{next-name}'
+PREVIOUS_FILE = '{previous-file}'
+PREVIOUS_NAME = '{previous-name}'
 
 LINK_FORMAT = '<a class="link" href="{0}">{1}</a><br/>\n'
-TITLE_FORMAT = '<span class="title" onload="load(this)" onclick="toggle(this)" data-text="{0}" data-translation="{1}">{0}</span>\n'
-LINE_FORMAT = '<div class="line" onload="load(this)" onclick="toggle(this)" data-text="{0}" data-translation="{1}">{0}</div>\n'
-EMPTY_LINE = '<br/>\n'
+TITLE_FORMAT = '        <div class="title" onload="load(this)" onclick="toggle(this)" data-text="{0}" data-translation="{1}">{0}</div>\n'
+LINE_FORMAT = '        <div class="line" onload="load(this)" onclick="toggle(this)" data-text="{0}" data-translation="{1}">{0}</div>\n'
+EMPTY_LINE = '        <br/>\n'
 
 COPY_FILES = [
   'lyrics.css',
@@ -21,14 +27,28 @@ def find(lines, text):
     if (text in line):
       return i + 1
 
+
+def toHtmlFileName(song):
+  return song.replace(' ', "_").lower() + '.html'
+
+
 if __name__ == '__main__':
   siteDir = path.join(path.dirname(sys.argv[0]), 'site')
 
   src = sys.argv[1]
   dst = sys.argv[2]
 
+  with open(path.join(siteDir, 'lyrics.html')) as f:
+    lyricsLines = f.readlines()
+
+  headerLines =  find(lyricsLines, '<div id="header">')
+  contentLines = find(lyricsLines, '<div id="content">')
+  previousLine = find(lyricsLines, PREVIOUS_FILE)
+  nextLine = find(lyricsLines, NEXT_FILE)
+
   links = []
-  for song in sorted(listdir(src)):
+  songs = sorted(listdir(src))
+  for (songNumber, song) in enumerate(songs):
     dir = path.join(src, song)
 
     with open(path.join(dir, "original.txt")) as f:
@@ -40,16 +60,13 @@ if __name__ == '__main__':
       print 'Line mismatch. Aborting'
       exit(1)
 
-    with open(path.join(siteDir, 'lyrics.html')) as f:
-      lines = f.readlines()
 
+    lines = copy.deepcopy(lyricsLines)
 
-    at =  find(lines, '<div id="header">')
-
-    lines.insert(at, TITLE_FORMAT.format(originalLines[0], translatedLines[0]))
-    at =  find(lines, '<div id="content">')
+    lines.insert(headerLines, TITLE_FORMAT.format(originalLines[0].strip(), translatedLines[0].strip()))
+    at = contentLines + 1  # Because we inserted a title line.
     maxLine = 0
-    for (i, line) in enumerate(originalLines[1:]):
+    for (lineNumber, line) in enumerate(originalLines[1:]):
       line = line.strip()
       if len(line) > maxLine:
         maxLine = len(line)
@@ -57,15 +74,28 @@ if __name__ == '__main__':
       if line == '':
         lines.insert(at, EMPTY_LINE)
       else:
-        translatedLine = translatedLines[i + 1].strip()
+        translatedLine = translatedLines[lineNumber + 1].strip()
         if len(translatedLine) > maxLine:
           maxLine = len(translatedLine)
         lines.insert(at, LINE_FORMAT.format(line, translatedLine))
       at += 1
 
 
-    htmlFilename = song.replace(' ', "_").lower() + '.html'
+    htmlFilename = toHtmlFileName(song)
     links += LINK_FORMAT.format(htmlFilename, song)
+
+    if songNumber == len(songs) - 1:
+      del lines[nextLine]
+    else:
+      nextSong = songs[songNumber  + 1]
+      lines[nextLine] = lines[nextLine].replace(NEXT_NAME, nextSong).replace(NEXT_FILE, toHtmlFileName(nextSong))
+
+    if songNumber  == 0:
+      del lines[previousLine]
+    else:
+      previousSong = songs[songNumber  - 1]
+      lines[previousLine] = lines[previousLine].replace(PREVIOUS_NAME, previousSong).replace(PREVIOUS_FILE, toHtmlFileName(previousSong))
+
     with open(path.join(dst, htmlFilename), 'w') as f:
       f.writelines(lines)
 
